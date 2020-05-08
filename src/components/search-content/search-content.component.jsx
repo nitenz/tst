396e67fbba0e3137cc855a47ff3093d67cefb7b6
nitenz <nitenz@gmail.com>
2020-05-08 08:26:30 +0100

    Changes

---
+++ src/components/search-content/search-content.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './search-content.styles.scss';
+import Form from 'react-bootstrap/Form';
+
+const SelectComponent = ({ optionsList, handleClick}) => (
+  <div>
+    <Form >
+        <Form.Group controlId="exampleForm.SelectCustom">
+          <Form.Label>Movie List</Form.Label>
+            <Form.Control onChange={handleClick} as="select" custom>
+            <option defaultValue>Select movie</option>
+              {
+                optionsList.map((item) =>(
+                  <optgroup key={item.id} label={item.type}>
+                    {
+                        item.videoList.map((movie) => (
+                          <option key={movie.id} >{movie.name}</option>
+                        ))
+                    }
+                  
+                  </optgroup>
+                ))
+              }
+            </Form.Control>
+        </Form.Group>
+        </Form>
+  </div>
+)
+
+export default SelectComponent;
---
+++ src/components/user-data/user-data.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './user-data.styles.scss';
+
+const UserInfo = ({userData: {username, isWatching, name, type, timeWatched } }) => (
+    <div className={isWatching ? 'user-data show' : 'user-data hide'}>
+        <div>
+            <label>{username} is watching {name} its a {type} movie </label><br></br>
+            <label id="timer" ></label>
+        </div>
+    </div>
+)
+
+export default UserInfo;
